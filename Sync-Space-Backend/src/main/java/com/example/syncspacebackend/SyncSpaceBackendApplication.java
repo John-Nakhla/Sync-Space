e7df2c47af7e2587755289cbb9f5494689f56b41
John-Nakhla <waeljohn306@gmail.com>
2026-03-15 17:30:44 +0200

    backend project structure

---
+++ Sync-Space-Backend/src/main/java/com/example/syncspacebackend/SyncSpaceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.syncspacebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyncSpaceBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SyncSpaceBackendApplication.class, args);
+    }
+
+}
---
+++ Sync-Space-Backend/src/test/java/com/example/syncspacebackend/SyncSpaceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.syncspacebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyncSpaceBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
