dfb732544e0bbda3a41a62ee34d4471b800836c9
Rawan Ibraheem <rawanebraheem16@gmail.com>
2026-03-27 03:37:46 +0200

    added user controller and attached token to axios

---
+++ Sync-Space-Backend/src/main/java/com/example/syncspacebackend/controllers/AuthController.java
@@ -0,0 +1,28 @@
+package com.example.syncspacebackend.controllers;
+
+import com.example.syncspacebackend.dtos.LoginRequest;
+import com.example.syncspacebackend.dtos.RegisterRequest;
+import com.example.syncspacebackend.services.AuthService;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody RegisterRequest request) {
+        return ResponseEntity.ok(authService.register(request));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody LoginRequest request) {
+        return ResponseEntity.ok(authService.login(request));
+    }
+}
--- sync-space-frontend/src/pages/Login.jsx
+++ sync-space-frontend/src/pages/Login.jsx
@@ -1,5 +1,33 @@
-function Login() {
-  return <h1>Login Page</h1>;
-}
+import axios from "axios";
+import api from "../api/api";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
-export default Login;
+function Login() {
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleLogin = async () => {
+    try {
+      const response = await api.post(
+        "/api/auth/login",
+        {
+          email: email,
+          password: password,
+        }
+      );
+
+      const token = response.data.token;
+
+      localStorage.setItem("token", token);
+
+      navigate("/dashboard");
+
+    } catch (err) {
+      setError("Invalid email or password");
+    }
+  };
+}
